b75d0dbc7f979a9f692fe1053380e0c1c6408c41
Sarah Caplan <caplan.sarah@gmail.com>
2017-01-11 08:20:08 -0700

    Added comments to organize all code

--- blog-short-for-weblog.py
+++ blog-short-for-weblog.py
@@ -1,132 +1,110 @@
 import os
-
 import jinja2
 import webapp2
-
+import cgi
 import re
 
+# Methods for developing with Google Datastore
+from google.appengine.ext import db
+
+# File paths for pointing to HTML Templates
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+
+#Envoking Jinja2
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
-class Handler(webapp2.RequestHandler):
+#### Begin blog code
+
+# Google Datastore entities i.e. db tables
+class Article(db.Model):
+  title = db.StringProperty(required = True)
+  body = db.TextProperty(required = True)
+  created = db.DateTimeProperty(auto_now_add = True)
+
+# Commonly used functions
+class BaseHandler(webapp2.RequestHandler):
   def write(self, *a, **kw):
     self.response.write(*a, **kw)
 
+  def render_str(self, template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
   def render(self, template, **kw):
-    self.write(template, **kw))
+    self.write(self.render_str(template, **kw))
 
-class SignUp(Handler):
+# Home page of blog display 10 latest entries
+class Home(BaseHandler):
+  def get(self, title="", body=""):
+    articles = db.GqlQuery("SELECT * FROM Article ORDER BY created DESC LIMIT 10")
+    self.render("home.html", title=title, body=body, articles=articles)
+
+# Page for creating new posts. Successful post redirects to post's permalink location.
+class newPost(BaseHandler):
+  def render_new(self, title="", body="", error=""):
+    self.render("newpost.html", title=title, body=body, error=error)
+    pass
+
   def get(self):
-    items = self.request.get_all("food")
-    self.render("signup.html", items = items)
+    self.render_new()
 
-class Welcome(webapp2.RequestHandler):
+  def post(self):
+    title = self.request.get("title")
+    body = self.request.get("body")
+
+    if title and body:
+      article = Article(title = title, body=body)
+      article.put()
+      article_id = key.id()
+      self.redirect("/<article_id>")
+    else:
+      error = "we need both a title and some text!"
+      self.render_new(title, body, error = error)
+
+# Handler for post's pages. Creates permalink.
+class Post(BaseHandler):
   def get(self):
-  self.render("welcome.html", username = username)
+    article = db.GqlQuery("SELECT * FROM Article WHERE ID = article_id")
+    self.render("post.html", title, body, article)
+
+# class SignUp(BaseHandler):
+#   def get(self):
+#     self.render("signup.html")
+#     # username = val_un
+#     # password = val_pw
+#     # email = val_em
+#    #  self.render('signup.html', username = username, password = password, verify = verify, email = email)
+
+#   def post(self):
+#     user_username = self.request.get('username')
+#     self.valid_username(user_username)
+#     # user_password = self.request.get('password')
+#     # user_verify = self.request.get('verify')
+#     # user_email = self.request.get('email')
+
+#   def valid_username(user_username):
+#     if USER_RE.match(user_username):
+#       username = user_username
+#     else:
+#       get("signup.html", username = user_username)
+#       self.write(error_username)
+
+#   def complete():
+#     if(username, password, email):
+#       self.redirect("/thanks")
+
+# class Welcome(BaseHandler):
+#   def get(self):
+#     self.render("welcome.html")
 
 
+# URI to Handler mapping
 app = webapp2.WSGIApplication([
-        ('/', SignUp),
-        ('/welcome', Welcome)
-        ],
-        debug=True)
+  ('/', Home),
+  ('/newpost', newPost ),
+  ('/<article_id>', Post)
+  # ('/welcome', Welcome)
+  ],
+  debug=True)
 
-# import webapp2
-# import cgi
 
-# form="""
-# <form method ="post">
-# 	What is your birthday
-# 	<br>
-# 	<label>
-# 		Month
-# 		<input type="text" name="month" value="%(month)s">
-# 	</label>
-# 	<label>
-# 		Day
-# 		<input type="text" name="day" value="%(day)s">
-# 	</label>
-# 	<label>
-# 		Year
-# 		<input type="text" name="year" value="%(year)s">
-# 	</label>
-# 	<div style="color: red">%(error)s</div>
-# 	<br>
-# 	<br>
-# 	<input type="submit">
-# </form>
-# """
-# def escape_html(s):
-#   return cgi.escape(s, quote = True)
-
-# class MainPage(webapp2.RequestHandler):
-#     def write_form(self, error="", month="", day="", year=""):
-#     	self.response.write(form % {"error" : error,
-#     								"month" : escape_html(month),
-#     								"day": escape_html(day),
-#     								"year": escape_html(year)})
-
-#     def get(self):
-#         # self.response.headers['Content-Type'] = 'text/plain'
-#         self.write_form()
-
-#     def post(self):
-#     	user_month = self.request.get('month')
-#     	user_day = self.request.get('day')
-#     	user_year = self.request.get('year')
-
-#     	month = valid_month(user_month)
-#     	day = valid_day(user_day)
-#     	year = valid_year(user_year)
-
-#     	if not (month and day and year):
-#     		self.write_form("That does not look like a valid date.", user_month, user_day, user_year)
-#     	else:
-#     		self.redirect("/thanks")
-
-# class ThanksHandler(webapp2.RequestHandler):
-#   def get (self):
-#       self.response.write("Thank you!")
-
-# # class TestHandler(webapp2.RequestHandler):
-# # 	def post(self):
-# # 		q = self.request.get("q")
-# # 		self.response.write(q)
-
-# months = ['January',
-#           'February',
-#           'March',
-#           'April',
-#           'May',
-#           'June',
-#           'July',
-#           'August',
-#           'September',
-#           'October',
-#           'November',
-#           'December']
-
-# month_abbvs = dict((m[:3].lower(),m) for m in months)
-# def valid_month(month):
-# 	if month:
-# 		cap_month = month.capitalize()
-# 		if cap_month in months:
-# 			return cap_month
-
-# def valid_day(day):
-# 	if day and day.isdigit():
-# 		day = int(day)
-# 		if day > 0 and day <= 31:
-# 			return day
-
-# def valid_year(year):
-# 	if year and year.isdigit():
-# 		year = int(year)
-# 		if year >1900 and year <2020:
-# 			return year
-
-# app = webapp2.WSGIApplication([
-#     ('/', MainPage),
-#     ('/thanks', ThanksHandler)
-#     # ('/testform', TestHandler)
-# ], debug=True)
